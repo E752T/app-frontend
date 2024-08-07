b3bab7efa562706c1ae5292222d115ea2a2c2893
E725T <c3pO47@proton.me>
2024-08-07 12:34:35 +0200

    update mock module.ts

--- src/app/geographical-origins/geographical-origins.component.ts
+++ src/app/geographical-origins/geographical-origins.component.ts
@@ -1,14 +1,115 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  input,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { IonModal } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 
-import { EditorsComponent } from './editors.component';
+import { baseURL, today } from '../services/data.service';
+import { PostRequest } from '../services/request.service';
+import { OverlayEventDetail } from '@ionic/core';
+import { Publisher } from '../services/interfaces.service';
+import { HttpClient } from '@angular/common/http';
 
-@NgModule({
-  declarations: [EditorsComponent],
-  imports: [CommonModule, FormsModule, IonicModule, RouterModule],
-  exports: [EditorsComponent],
+@Component({
+  selector: 'app-editors',
+  templateUrl: './editors.component.html',
+  styleUrls: ['./editors.component.scss'],
 })
-export class EditorsModule {}
+export class EditorsComponent {
+  [x: string]: any;
+
+  constructor(
+    private http: HttpClient,
+    private modalController: ModalController
+  ) {}
+
+  private platform = inject(Platform);
+
+  @Input()
+  publisher!: Publisher;
+
+  @Input()
+  publishers!: Array<Publisher>;
+
+  @Input()
+  search_input!: string | null | undefined;
+
+  @Output()
+  updatePublishers = new EventEmitter<any>();
+
+  @ViewChild(IonModal)
+  modal!: IonModal;
+
+  modalCtrl: any;
+
+  bodyAddPublisher: Publisher = {
+    publisherID: 0,
+    name: '',
+    addedDate: today,
+    lastUpdateDate: today,
+    description: '',
+    email: '',
+    telephone1: '',
+    telephone2: '',
+    notes: '',
+  };
+
+  bodyModifyPublisher: Publisher = {
+    publisherID: 0,
+    name: '',
+    addedDate: today,
+    lastUpdateDate: today,
+    description: '',
+    email: '',
+    telephone1: '',
+    telephone2: '',
+    notes: '',
+  };
+
+  DeleteElement(objectID: any) {
+    this.publishers.filter(
+      (element: Publisher) => element.publisherID !== objectID
+    );
+    var elementToDelete = console.log(this.publishers);
+    this.updatePublishers.emit(elementToDelete);
+    return PostRequest(baseURL + 'DeleteCategory/' + objectID);
+  }
+
+  confirmDeleteElement() {
+    this.DeleteElement(this.publisher?.publisherID);
+    this.modalCtrl.dismiss({ confirmed: true });
+  }
+
+  UpdateAPI(): Promise<any> {
+    return PostRequest(baseURL + 'UpdatePublisher/', this.publisher);
+  }
+
+  getNewID(elementList: Array<Publisher>): number {
+    let highestID = 0;
+    for (let i = 0; i < elementList.length; i++) {
+      if (elementList[i].publisherID > highestID) {
+        highestID = elementList[i].publisherID;
+      }
+    }
+    return highestID + 1;
+  }
+
+  confirm() {
+    this.modalController.dismiss({ confirmed: true });
+  }
+
+  cancel() {
+    this.modalController.dismiss({ confirmed: false });
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+  }
+}
