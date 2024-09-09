d823e5f61bfe8b5f2067271ba7873b609d9f1ae4
E725T <c3pO47@proton.me>
2024-09-09 18:19:56 +0200

    login-page css

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
+import { LoginObject } from '../services/interfaces.service';
+import { PostRequest } from '../services/request.service';
+import { baseURL } from '../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +12,6 @@ import { LoadingController } from '@ionic/angular';
 export class LoginPage implements OnInit {
   public errorMessage: string | undefined;
 
-  body_login: any;
-  confirmLogin: any;
   modalCtrl: any;
 
   constructor(private loadingController: LoadingController) {}
@@ -18,7 +19,6 @@ export class LoginPage implements OnInit {
   cancel() {
     this.modalCtrl.dismiss(null, 'cancel');
   }
-  
   async ngOnInit() {
     // Controllo se ci sono parametri nella URL
     if (window.location.hash) {
@@ -37,6 +37,102 @@ export class LoginPage implements OnInit {
         loadingIndicator.dismiss();
       }
     }
+  }
+
+  body_login: LoginObject = {
+    email: 'admin',
+    password: 'admin',
+  };
+
+  token_JWT: string = '';
+  token_JWT_success: boolean = false;
+  username: string = '';
+  user_role: string = 'test';
+
+  showToast(message: string, color: string) {
+    const toast = document.createElement('ion-toast');
+    toast.message = message;
+    toast.color = color;
+    toast.duration = 3000;
+    document.body.appendChild(toast);
+    return toast.present();
+  }
+
+  async confirmLogin() {
+    try {
+      const response = await PostRequest(baseURL + 'Login/', this.body_login);
+
+      if (response != null && response != 404 && response != 'Empty values.') {
+        console.log('Login SUCCESS, JWT token: ', response);
+        this.token_JWT = response;
+
+        /// CHANGE THIS VALIDATION METHOD, I need to check the DB
+        // (this method is easy but unsecure)
+        if (this.token_JWT.length > 50) {
+          this.token_JWT_success = true;
+          console.log('LOGIN SUCCESS ', this.token_JWT_success);
+          this.modalCtrl.dismiss(this.body_login.email, 'confirm');
+          this.showToast('Accesso Eseguito', 'success');
+          this.username = this.body_login.email;
+        }
+      } else {
+        console.warn('Login failed: No valid string received.');
+        this.token_JWT_success = false;
+        this.showToast('Credenziali Errate', 'danger');
+      }
+      this.user_role = this.getUserRole();
+      console.log("RUOLO DELL' UTENTE ", this.user_role);
+    } catch (error) {
+      console.error('An error occurred during login: ', error);
+      this.token_JWT_success = false;
+      this.showToast('Errore durante il login', 'danger');
+    } finally {
+      this.body_login.email = '';
+      this.body_login.password = '';
+    }
+  }
+
+  jwt_decode(token: string): any {
+    // Verifica se il token è valido
+    if (!token) {
+      throw new Error('Token non fornito');
+    }
+
+    // Divide il token in parti
+    const parts = token.split('.');
+    if (parts.length !== 3) {
+      throw new Error('Token non valido');
+    }
+
+    // Decodifica la parte payload
+    const payload = parts[1];
+    const decodedPayload = JSON.parse(atob(payload));
+    console.log(decodedPayload);
+    return decodedPayload;
+  }
+
+  getUserRole(): string {
+    const token = this.token_JWT;
+
+    if (token) {
+      if (token.includes('admin')) {
+        return 'admin';
+      } else {
+        return 'user';
+      }
+    } else {
+      return 'token not found';
+    }
+  }
+
+  exit_login() {
+    this.body_login.email = '';
+    this.body_login.password = '';
+    this.token_JWT = '';
+    this.token_JWT_success = false;
+    this.username = '';
+    this.user_role = '';
+    this.modalCtrl.dismiss(this.body_login, 'confirm');
   }
 
   async login() {
