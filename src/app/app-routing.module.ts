6ad51994cc674f94f2edeb7d0181988850b094b7
E725T <c3pO47@proton.me>
2024-09-12 16:59:24 +0200

    routing for message:id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { MessageComponentModule } from './message/message.module';
+
 import { AuthGuard } from './auth-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' }, // Reindirizza alla pagina di login
+  {
+    path: '',
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule), // Importa il modulo della home
+  },
   {
     path: 'login',
     loadChildren: () =>
       import('./login/login.module').then((m) => m.LoginPageModule), // Importa il modulo del login
   },
   {
-    path: '',
-    loadChildren: () =>
-      import('./home/home.module').then((m) => m.HomePageModule), // Importa il modulo della home
+    path: 'messages',
+    component: MessageComponentModule,
+    children: [{ path: ':id', component: MessageComponentModule }],
   },
 ];
 
