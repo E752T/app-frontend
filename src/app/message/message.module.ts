25bfdd1ccaf4736f96d10bcbe098b41e6412d9de
E725T <c3pO47@proton.me>
2024-09-12 16:43:55 +0200

    generate objects profile

--- src/app/message/message.module.ts
+++ src/app/message/message.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { MessageComponent } from './message.component';
+const routes: Routes = [
+  { path: '', component: MessageComponent },
+  { path: ':id' },
+];
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, RouterModule],
