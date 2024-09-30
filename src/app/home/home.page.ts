1c7d54b944dd0b604c6bf1e559c6bd6d6027c50c
E725T <c3pO47@proton.me>
2024-09-30 17:04:25 +0200

    Reapply "reformatting code pt2"
    This reverts commit 49c3c4a361446069b01855879c31a7380e11d33c.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { ModalController, RefresherCustomEvent } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 import { ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MenuController } from '@ionic/angular';
@@ -1086,12 +1086,6 @@ export class HomePage implements OnInit {
     } else {
       return result;
     }
-  }
-
-  refresh(ev: any) {
-    setTimeout(() => {
-      (ev as RefresherCustomEvent).detail.complete();
-    }, 3000);
   }
 
   async updateCredentials() {
